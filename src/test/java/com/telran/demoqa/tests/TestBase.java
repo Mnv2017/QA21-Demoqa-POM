86da253e53dc25c32aa0f307e1353b54ce98396f
Mnv2017 <mnv2017@gmx.de>
2021-12-01 19:03:56 +0100

    Homework 01.12.21 added Logger

--- src/test/java/com/telran/demoqa/tests/TestBase.java
+++ src/test/java/com/telran/demoqa/tests/TestBase.java
@@ -2,24 +2,46 @@ package com.telran.demoqa.tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.internal.TestResult;
 
+import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
     WebDriver driver;
 
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
     @BeforeMethod
-    public void setUp(){
-        driver=new ChromeDriver();
+    public void setUp() {
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://demoqa.com");
     }
 
+    @BeforeMethod
+    public void startTest(Method m) {
+        logger.info("### Start test" + m.getName());
+    }
+
     @AfterMethod
-    public void tearDown(){
+    public void stopTest(ITestResult result) {
+        if (result.isSuccess()) {
+            logger.info("### PASSED: test method " + result.getMethod().getMethodName());
+        } else {
+            logger.error("FAILED: test method " + result.getMethod().getMethodName());
+        }
+        System.out.println("===========================");
+    }
+
+    @AfterMethod
+    public void tearDown() {
         driver.quit();
     }
 
